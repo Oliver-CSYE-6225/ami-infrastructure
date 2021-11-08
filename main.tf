74295d80f93a66de8b9ee1e83b5836c9cef96434
Olly96 <oliverrodrigues99@gmail.com>
2021-11-08 15:27:11 -0500

    udate region

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_iam_policy" "gh-ec2-ami" {
   name        = "gh-ec2-ami"
   description = "gh-ec2-ami"
-
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -33,7 +32,14 @@ resource "aws_iam_policy" "gh-ec2-ami" {
                 "ec2:DeleteSecurityGroup",
                 "ec2:CreateSnapshot",
                 "ec2:ModifyInstanceAttribute",
-                "ec2:DeleteKeyPair".
+                "ec2:DeleteKeyPair"
+            ],
+            "Resource": "arn:aws:ec2:us-east-1:746774523931:*/*"
+        },
+        {
+            "Sid": "VisualEditor2",
+            "Effect": "Allow",
+            "Action": [
                 "ec2:DescribeImages",
                 "ec2:DescribeInstances",
                 "ec2:DescribeTags",
@@ -44,15 +50,40 @@ resource "aws_iam_policy" "gh-ec2-ami" {
                 "ec2:DescribeSecurityGroups",
                 "ec2:DescribeInstanceStatus"
             ],
-            "Resource": "arn:aws:ec2:us-east-1:746774523931:*/*"
+            "Resource": "*"
         }
     ]
 }
   EOF
 }
 
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          AWS =  "arn:aws:iam::746774523931:user/ghactions-ami"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
 resource "aws_iam_policy_attachment" "attach-gh-ec2-ami" {
   name       = "attach-gh-ec2-ami"
-  users      = ["ghactions-ami"]
+  // users      = ["ghactions-ami"]
+  roles      = [aws_iam_role.test_role.name]
   policy_arn = aws_iam_policy.gh-ec2-ami.arn
 }
